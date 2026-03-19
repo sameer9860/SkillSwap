6ca9ab1dcd617890653b8275dfb07918611d51f1
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-19 21:23:56 +0545

    Added user model

---
+++ backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  role: {
+    type: String,
+    enum: ["student", "mentor", "admin"],
+    default: "student",
+  },
+
+  bio: String,
+
+  skillsOffered: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Skill",
+    },
+  ],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
