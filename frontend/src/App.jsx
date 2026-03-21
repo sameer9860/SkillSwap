fb398f29dfaf3fe367f12cb053bfefa154120339
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-21 20:56:21 +0545

    Added routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import SkillDetails from './pages/SkillDetails';
+import PrivateRoute from './routes/PrivateRoute';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } 
+        />
+        <Route path="/skills/:id" element={<SkillDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
