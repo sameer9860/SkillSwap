2d343e0ee687ac99b56dd0abf0f69ec1e81ab255
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-21 20:58:20 +0545

    Added frontnd

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to SkillSwap</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/SkillDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SkillDetails = () => {
+  return (
+    <div>
+      <h1>Skill Details</h1>
+    </div>
+  );
+};
+
+export default SkillDetails;
